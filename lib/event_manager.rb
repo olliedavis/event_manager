5a00e99895b494e36ee7eab3ed4e7db338301575
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-01-06 05:59:50 +0000

    Now outputs to seperate HTML Files

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,43 +2,48 @@ require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
 
-template_letter = File.read "form_letter.html"
-
+lib = "/home/ollie/event_manager/"
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
 end
 
-def legislators_by_zipcode(zipcode)
+def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
-    legislators = civic_info.representative_info_by_address(
-                              address: zipcode,
-                              levels: 'country',
-                              roles: ['legislatorUpperBody', 'legislatorLowerBody'])
-    legislators = legislators.officials
-    legislator_names = legislators.map(&:name)
-    legislator_names.join(", ")
+    civic_info.representative_info_by_address(
+        address: zip,
+        levels: 'country',
+        roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    ).officials
   rescue
       "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
   end
 end
 
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir("output") unless Dir.exist? "output"
+  filename = "output/thanks_#{id}.html"
+  File.open(filename,'w') do |file|
+    file.puts form_letter
+  end
+end
+
 puts "EventManager Initialized!"
 
-contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents = CSV.open "#{lib}/event_attendees.csv", headers: true, header_converters: :symbol
+
+template_letter = File.read "#{lib}/form_letter.erb"
+erb_template = ERB.new template_letter
 
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
-
   zipcode = clean_zipcode(row[:zipcode])
-
   legislators = legislators_by_zipcode(zipcode)
+  form_letter = erb_template.result(binding)
+  save_thank_you_letter(id, form_letter)
 
-  personal_letter = template_letter.gsub('FIRST_NAME',name)
-  personal_letter.gsub!('LEGISLATORS', legislators)
-
-  puts personal_letter
 end
