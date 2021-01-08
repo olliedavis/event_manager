fdee6395a34cd17ae07912600d00b28917b2274b
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-01-08 06:23:42 +0000

    Assignment Completed

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -8,13 +8,16 @@ $hours_arr = Array.new
 $days_arr = Array.new
 
 contents = CSV.open "../event_attendees.csv", headers: true, header_converters: :symbol
-#template_letter = File.read "../form_letter.erb"
-#erb_template = ERB.new template_letter
+template_letter = File.read "../form_letter.erb"
+erb_template = ERB.new template_letter
 
+# if the zipcode is less than 5 numbers, 0's are added to the beginning until does. 
+# if the zipcode is more than 5 numbers, it returns on the first 5 digits.
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
 end
 
+# strips non-integers from the number and checks to see if it's a valid number
 def clean_number(number)
   number = number.delete("^0-9").to_s
   if number.length == 11 and number[0] == "1"
@@ -24,6 +27,7 @@ def clean_number(number)
   end
 end
 
+##  pulls the representative info and matches them to each user by zipcode
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -39,6 +43,7 @@ def legislators_by_zipcode(zip)
   end
 end
 
+# creates a directory in the project folder, creates a HTML files, opens it, and then puts the ERB template inside
 def save_thank_you_letter(id, form_letter)
   Dir.mkdir("output") unless Dir.exist? "output"
   filename = "output/thanks_#{id}.html"
@@ -47,19 +52,25 @@ def save_thank_you_letter(id, form_letter)
   end
 end
 
+# iterates through the CSV file, assigning each required column a variable.
 contents.each do |row|
+
   id = row[0]
-  name = row[:first_Name]
+  name = row[:first_name]
   time = row[:regdate]
   date = DateTime.strptime(time, '%m/%d/%y %H:%M')
+
 
   $hours_arr.push(date.hour)
   $days_arr.push(date.wday)
   phone = clean_number(row[:homephone])
-  #zipcode = clean_zipcode(row[:zipcode])
-  #legislators = legislators_by_zipcode(zipcode)
-  #form_letter = erb_template.result(binding)
-  #save_thank_you_letter(id, form_letter)
+  zipcode = clean_zipcode(row[:zipcode])
+  legislators = legislators_by_zipcode(zipcode)
+  
+  form_letter = erb_template.result(binding)
+  save_thank_you_letter(id, form_letter)
+  puts "Thank you letter created for #{name}"
+  
 end
 
 # returns the mode average of the hour of signups
@@ -71,7 +82,7 @@ def average_sign_up_time(hours_arr)
   mode_hours_hash.key(mode_hours_hash.values.max)
 end
 
-
+#returns the the mode average day of sign ups
 def average_sign_up_days(days_arr)
   mode_days_hash = Hash.new(0)
   $days_arr.each do |int|
